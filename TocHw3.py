09a4acfe4bd5f4e132f38666595b49413c581c0a
tkloong <tkloong92@yahoo.com>
2014-06-06 22:41:52 +0800

    Streamlining the code

--- TocHw3.py
+++ TocHw3.py
@@ -6,29 +6,21 @@
 
 import sys
 import urllib
-from lxml import html
 import json
-import time
 
-start = time.clock()
 if len(sys.argv) != 5:
 	print 'Usage: {0} <URL> <鄉鎮市區> <Road_Name> <year>'.format(sys.argv[0])
 	sys.exit()
 
-sys.argv[4] = int(sys.argv[4]+"01")
-url = sys.argv[1]
-page = urllib.urlopen(url)
-data = json.load(page)
+sys.argv[4] = int(sys.argv[4]+"01")		# Year + month
+data = json.load(urllib.urlopen(sys.argv[1]))
 
-matches = 0
-total = 0
+total = matches = 0
 for datum in data:
 	if datum[u'鄉鎮市區'] and datum[u'土地區段位置或建物區門牌'] and sys.argv[2] == datum[u'鄉鎮市區'].encode('utf-8') and sys.argv[3] in datum[u'土地區段位置或建物區門牌'].encode('utf-8') and sys.argv[4] <= datum[u'交易年月']:
 		total += datum[u'總價元']
 		matches += 1
 
-stop = time.clock()
-print stop - start
 if matches:
 	print total / matches
 else:
